e2ef3058ba3cf1706ee5e9f9774a0276104b58af
jcornelussen <j.cornelussen@planet.nl>
2018-12-04 22:29:20 +0100

    Add new item functionality complete.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -11,14 +11,14 @@ import UIKit
 class TodoListViewController: UITableViewController {
   
   
-  let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+  var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
   
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
   
-  //Mark: - Tableview Datasource Methods
+  //MARK: - Tableview Datasource Methods
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return itemArray.count
   }
@@ -31,7 +31,7 @@ class TodoListViewController: UITableViewController {
     return cell
   }
   
-  //Mark: - Tableview Delegate Methods
+  //MARK: - Tableview Delegate Methods
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     print(indexPath.row)
     
@@ -45,6 +45,35 @@ class TodoListViewController: UITableViewController {
     }
     
     tableView.deselectRow(at: indexPath, animated: true)
+  }
+  
+  //MARK: - Add New Items
+  
+  @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    
+    var textField = UITextField()
+    
+    let alert = UIAlertController(title: "Add new Todoey Item", message: "", preferredStyle: .alert)
+    let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+      // What will happen once the user clicks the Add Item button on our UIAlert
+      
+      self.itemArray.append(textField.text!)
+      
+      // reload the table to display the new item in the tableview.
+      self.tableView.reloadData()
+    }
+    
+    alert.addTextField { (alertTextField) in
+      alertTextField.placeholder = "Create new item"
+      textField = alertTextField
+    }
+    
+    // Add above action to the UI Alert Controller.
+    alert.addAction(action)
+    
+    // Show the UI Alert Controller on screen.
+    present(alert, animated: true, completion: nil)
+    
   }
   
 }
